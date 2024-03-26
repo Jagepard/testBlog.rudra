2dee4e42d3384df2d392758908ffecc0f600e827
Jagepard <dankorot@gmail.com>
2024-03-26 12:48:36 +0300

    update

--- app/Ship/config.php
+++ app/Ship/config.php
@@ -1,22 +1,20 @@
 <?php
 
 use Rudra\Container\Rudra;
-use Rudra\Container\Request;
 use DebugBar\StandardDebugBar;
 use Rudra\Container\Interfaces\RudraInterface;
-use Rudra\Container\Interfaces\RequestInterface;
 
 return [
-    'contracts'   => [
-        RudraInterface::class   => Rudra::run(),
-        // RequestInterface::class => Request::class
+    'contracts' => [
+        RudraInterface::class => Rudra::run(),
     ],
-    'services'    => [
+
+    'services'  => [
         "DSN" => [PDO::class, [
-            Rudra::run()->config()->get('database')['dsn'], 
-            Rudra::run()->config()->get('database')['username'], 
-            Rudra::run()->config()->get('database')['password']]
-        ],
+            config('database', 'dsn'),
+            config('database', 'username'),
+            config('database', 'password')
+        ]],
         "debugbar" => StandardDebugBar::class
     ]
 ];
