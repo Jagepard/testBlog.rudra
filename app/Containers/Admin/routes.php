a37be57f28a118e224d972b0b5c2dc1c2acdc0d9
Jagepard <jagepard@yandex.ru>
2025-07-07 11:59:20 +0300

    update Router::get

--- app/Containers/Admin/routes.php
+++ app/Containers/Admin/routes.php
@@ -5,12 +5,10 @@ use Rudra\Router\RouterFacade as Router;
 use Rudra\EventDispatcher\EventDispatcherFacade as Dispatcher;
 
 if (php_sapi_name() != "cli") {
-    Router::get([
-        'url' => "callable/:name",
-        'controller' => function ($name) {
+    Router::get("callable/:name", function ($name) {
             echo "Hello $name!";
         }
-    ]);
+    );
 }
 
 return [
